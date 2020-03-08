f46f687641eabc3d2df8602a574cb371529ac850
szywyk <47740194+szywyk@users.noreply.github.com>
2020-03-08 22:01:35 +0100

    Update cars.js

--- 02-JSCoreConcepts/cars.js
+++ 02-JSCoreConcepts/cars.js
@@ -1,5 +1,9 @@
 function isCar(car1, car2, car3) {
-    // return 
+    if (car1==true && car2==true && car3==true)
+        return true
+    else if (car1==false && car2==false && car3==false)
+        return true
+    else return false
 }
 
 console.log(isCar(true, true, true));
