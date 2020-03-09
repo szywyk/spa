7b821245d580d73e80bf162b6f68c0039a60dc1e
szywyk <47740194+szywyk@users.noreply.github.com>
2020-03-09 14:47:18 +0100

    Update university.js

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -1,24 +1,33 @@
+  
 names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
 
 class Students {
     constructor(names) {
-        // ...
+        this.names = names
     }
 
     startWith(letter){
-        return // names (array) which starts with the letter
+        let list = []
+        this.names.forEach(element => {
+            if (element[0]==letter)
+                list.push(element)
+        });
+        return list // names (array) which starts with the letter
     }
 
     sort() {
-        return // names (array) in alphabetical order 
+        return this.names.sort() // names (array) in alphabetical order 
     }
 
     get() {
-        return // names (array) in natural order
+        return this.names // names (array) in natural order
     }
 
     getFirst(n) {
-        return // get first 'n' names (array) 
+        let list = []
+        for (let i=0; i<n; i++)
+            list.push(this.names[i])
+        return list // get first 'n' names (array) 
     }
 }
 
@@ -30,6 +39,3 @@ console.log(students.startWith('M').sort());
 console.log(students.sort().getFirst(4));
 console.log(students.startWith('M').sort().getFirst(2));
 console.log(students.get());
-
-
-
